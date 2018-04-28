3d45478562bbf712dab0cfb9afe971c7fa0578a8
= <=>
2018-04-28 18:08:18 +0600

    Меню под разное кол-во пунктов меню

--- menu.js
+++ menu.js
@@ -1,7 +1,25 @@
 var menuBtn = document.querySelector(".categ__headline"),
-	menu = document.querySelector(".categ__menu");
+	menu = document.querySelector(".categ__menu"),
+	menuItem = document.querySelectorAll(".menu-list__item"),
+	menuList = document.querySelector(".menu-list"),
+	windowWidth = document.body.clientWidth;
 
 menu.classList.remove("categ__menu--nojs");
-menuBtn.addEventListener("click", function() {
-	menu.classList.toggle("categ__menu--opened");
-});
+if(windowWidth <= 900) {
+	menuBtn.addEventListener("click", function() {
+		menu.classList.toggle("categ__menu--opened");
+	});
+}
+
+if(menuItem.length > 28) {
+	if(windowWidth >= 900){
+		menu.classList.add("categ__menu--extended");
+		menuList.classList.add("menu-list--extended");
+	} else if(windowWidth < 900){
+		menuBtn.addEventListener("click", function() {
+			menu.classList.toggle("categ__menu--extended");
+			menuList.classList.add("menu-list--extended");
+		});
+	}
+
+}
