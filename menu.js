4c5a44b8174eaf412519b5e8c86d1f9d156402b0
= <=>
2018-04-28 15:58:46 +0600

    Готовый без доп ul

--- menu.js
+++ menu.js
@@ -2,8 +2,6 @@ var menuBtn = document.querySelector(".categ__headline"),
 			menu = document.querySelector(".categ__menu");
 
 menu.classList.remove("categ__menu--nojs");
-if(document.body.clientWidth <= 899) {
-	menuBtn.addEventListener("click", function() {
-		menu.classList.toggle("categ__menu--opened");
-	});
-}
+menuBtn.addEventListener("click", function() {
+	menu.classList.toggle("categ__menu--opened");
+});
